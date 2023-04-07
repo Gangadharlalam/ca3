52c9ab6c12a87048f80bef0b4cfe97b76095df62
gangadhar_lpu <gangadharlalam710@gmail.com>
2023-04-07 11:34:09 +0530

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,20 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.ResourceBundle;
+
+public class App {
+    public boolean login_test(String uname, String pass) {
+
+        ResourceBundle r = ResourceBundle.getBundle("Config");
+        String username = r.getString("username");
+        String password = r.getString("password");
+
+        if (username.equals(uname) && password.equals(pass)) {
+            return true;
+        } else
+
+            return false;
+
     }
 }
